90f2a23feba8f326b401844150392c1b1f0bb5ac
extremeoftime <extremeoftime@gmail.com>
2018-03-30 21:45:59 +0300

    lesson 30.03

---
+++ Igenio/Untitled-1.py
@@ -0,0 +1,18 @@
+board = list(range(1,10))
+
+def drow_board(board):
+    for i in range(3):
+        print(board[0+i*3],board[1+i*3],board[2+i*3])
+
+def take_input(player_token):
+    valid=False
+
+while not valid:
+        player_answer = input("Куда поставим " + player_token+"? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print ("Некорректный ввод. Вы уверены, что ввели число?")
+	     continue
+
+if player_answer>=1 and player_answer<=9:
--- Igenio/trian.py
+++ Igenio/trian.py
@@ -2,22 +2,19 @@ from tkinter import Tk,Canvas,Text
 root = Tk()
 root.geometry('1000x400')
 
-
-
 c = Canvas(width=460,height=460,bg='grey100')
 c.pack()
-oval = c.create_oval(30,10,130,80,fill='grey60',outline="black")
-rect = c.create_rectangle(180,10,280,80,fill='grey50',outline="black")
-trian = c.create_polygon(330,80,380,10,430,80,fill='grey80',outline="black")
+oval = c.create_oval(30,10,130,80,fill='#99FFCC',outline="black")
+rect = c.create_rectangle(180,10,280,80,fill='red',outline="black")
+trian = c.create_polygon(330,80,380,10,430,80,fill='purple',outline="black")
 
 def trian_but(event):
-    b_trian = c.create_polygon(350,70,380,20,410,70,fill='grey1',outline="black")
+    b_trian = c.create_polygon(350,70,380,20,410,70,fill='blue',outline="black")
 
 def oval_but(event):
     c.delete(oval)
     c.create_text(70,40,fill="darkblue",font="Times 10",
-                        text="Here was oval.")
-                        
+                        text="Here was oval.")       
 
 def rect_but(event):
     c.delete(rect)
@@ -27,6 +24,5 @@ def rect_but(event):
 c.tag_bind(trian,"<Button-1>",trian_but)
 c.tag_bind(oval,"<Button-1>",oval_but)
 c.tag_bind(rect,"<Button-1>",rect_but)
-
 
 root.mainloop()
